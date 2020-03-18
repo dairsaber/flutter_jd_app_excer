db14ac2a9b362566c69ebf7dc8fdabff04d39c1c
dashabei <yinxinpz@163.com>
2020-03-18 14:38:33 +0800

    添加fluttertoast

--- lib/pages/tabs/Test.dart
+++ lib/pages/tabs/Test.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import '../../utils/reducer.dart';
+// import '../../utils/reducer.dart';
 import '../../utils/storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+// const List<int> testList = [1, 2, 3, 4, 5];
 
-const List<int> testList = [1, 2, 3, 4, 5];
+// final result = Reducer.reduce<List, int>(testList, (prev, current, index) {
+//   if (current % 2 == 0) {
+//     prev.add(current);
+//   }
+//   return prev;
+// }, []);
 
-final result = Reducer.reduce<List, int>(testList, (prev, current, index) {
-  if (current % 2 == 0) {
-    prev.add(current);
-  }
-  return prev;
-}, []);
-
-final result2 = Reducer.reduce<Map, int>(testList, (prev, current, index) {
-  prev[index] = current;
-  return prev;
-}, {});
+// final result2 = Reducer.reduce<Map, int>(testList, (prev, current, index) {
+//   prev[index] = current;
+//   return prev;
+// }, {});
 
 class Test extends StatefulWidget {
   Test({Key key}) : super(key: key);
@@ -54,64 +54,153 @@ class _TestState extends State<Test> {
 
   @override
   Widget build(BuildContext context) {
-    print(result);
-    print(result2);
-    return Container(
-      child: Column(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: this._saveTolocal,
-            child: Text("保存本地数据"),
-          ),
-          RaisedButton(
-            child: Text("本地存储中获取int数据"),
-            onPressed: () async {
-              final myInt = await StorageUtils.getValue<int>("int");
-              this.setState(() {
-                _int = myInt;
-              });
-            },
-          ),
-          RaisedButton(
-            child: Text("本地存储中获取doublue数据"),
-            onPressed: () async {
-              final myDouble = await StorageUtils.getValue<double>("double");
-              this.setState(() {
-                _double = myDouble;
-              });
-            },
-          ),
-          RaisedButton(
-            child: Text("本地存储中获取map数据"),
-            onPressed: () async {
-              final myMap = await StorageUtils.getValue<Map>("Map");
-              this.setState(() {
-                _map = myMap;
-              });
-            },
-          ),
-          RaisedButton(
-            child: Text("本地存储中获取List数据"),
-            onPressed: () async {
-              final myList = await StorageUtils.getValue<List>("List");
-              this.setState(() {
-                _list = myList;
-              });
-            },
-          ),
-          RaisedButton(
-            child: Text("清除所有数据"),
-            onPressed: () async {
-              await StorageUtils.clear();
-              this._init();
-            },
-          ),
-          Container(child: Text("int==>$_int")),
-          Container(child: Text("doublue==>$_double")),
-          Container(child: Text("Map==>$_map")),
-          Container(child: Text("List==>$_list")),
-        ],
-      ),
-    );
+    // print(result);
+    // print(result2);
+    return SingleChildScrollView(
+        child: Column(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: this._saveTolocal,
+          child: Text("保存本地数据"),
+        ),
+        RaisedButton(
+          child: Text("本地存储中获取int数据"),
+          onPressed: () async {
+            final myInt = await StorageUtils.getValue<int>("int");
+            this.setState(() {
+              _int = myInt;
+            });
+          },
+        ),
+        RaisedButton(
+          child: Text("本地存储中获取doublue数据"),
+          onPressed: () async {
+            final myDouble = await StorageUtils.getValue<double>("double");
+            this.setState(() {
+              _double = myDouble;
+            });
+          },
+        ),
+        RaisedButton(
+          child: Text("本地存储中获取map数据"),
+          onPressed: () async {
+            final myMap = await StorageUtils.getValue<Map>("Map");
+            this.setState(() {
+              _map = myMap;
+            });
+          },
+        ),
+        RaisedButton(
+          child: Text("本地存储中获取List数据"),
+          onPressed: () async {
+            final myList = await StorageUtils.getValue<List>("List");
+            this.setState(() {
+              _list = myList;
+            });
+          },
+        ),
+        RaisedButton(
+          child: Text("清除所有数据"),
+          onPressed: () async {
+            await StorageUtils.clear();
+            this._init();
+          },
+        ),
+        Container(child: Text("int==>$_int")),
+        Container(child: Text("doublue==>$_double")),
+        Container(child: Text("Map==>$_map")),
+        Container(child: Text("List==>$_list")),
+        OutlineButton(
+          onPressed: () async {
+            final result = await showDialog<bool>(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(actions: <Widget>[
+                    FlatButton(
+                        splashColor: Color.fromRGBO(255, 0, 0, 0.2),
+                        child: Text(
+                          "确定",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                        }),
+                    FlatButton(
+                        child: Text("取消"),
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        })
+                  ], title: Text("AlertDialog"), content: Text("AlertDialog"));
+                });
+            print(result);
+          },
+          child: Text("alertDialog"),
+        ),
+        OutlineButton(
+          onPressed: () {
+            Fluttertoast.showToast(
+                msg: "我是一个Toast", toastLength: Toast.LENGTH_SHORT,backgroundColor: Colors.red[300],textColor: Colors.tealAccent);
+          },
+          child: Text("Toast"),
+        ),
+      ],
+    ));
+    // return Container(
+    //   child: Column(
+    //     children: <Widget>[
+    //       RaisedButton(
+    //         onPressed: this._saveTolocal,
+    //         child: Text("保存本地数据"),
+    //       ),
+    //       RaisedButton(
+    //         child: Text("本地存储中获取int数据"),
+    //         onPressed: () async {
+    //           final myInt = await StorageUtils.getValue<int>("int");
+    //           this.setState(() {
+    //             _int = myInt;
+    //           });
+    //         },
+    //       ),
+    //       RaisedButton(
+    //         child: Text("本地存储中获取doublue数据"),
+    //         onPressed: () async {
+    //           final myDouble = await StorageUtils.getValue<double>("double");
+    //           this.setState(() {
+    //             _double = myDouble;
+    //           });
+    //         },
+    //       ),
+    //       RaisedButton(
+    //         child: Text("本地存储中获取map数据"),
+    //         onPressed: () async {
+    //           final myMap = await StorageUtils.getValue<Map>("Map");
+    //           this.setState(() {
+    //             _map = myMap;
+    //           });
+    //         },
+    //       ),
+    //       RaisedButton(
+    //         child: Text("本地存储中获取List数据"),
+    //         onPressed: () async {
+    //           final myList = await StorageUtils.getValue<List>("List");
+    //           this.setState(() {
+    //             _list = myList;
+    //           });
+    //         },
+    //       ),
+    //       RaisedButton(
+    //         child: Text("清除所有数据"),
+    //         onPressed: () async {
+    //           await StorageUtils.clear();
+    //           this._init();
+    //         },
+    //       ),
+    //       Container(child: Text("int==>$_int")),
+    //       Container(child: Text("doublue==>$_double")),
+    //       Container(child: Text("Map==>$_map")),
+    //       Container(child: Text("List==>$_list")),
+    //     ],
+    //   ),
+    // );
   }
 }
